db7ae208709c9cd3c6a411781ccab3c46c5f81d1
tuanhuynh0209 <135773362+tuanhuynh0209@users.noreply.github.com>
2024-11-17 12:03:46 +0700

    image blog

--- src/components/pages/Blog.tsx
+++ src/components/pages/Blog.tsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import '../../styles/blog.css';
-import { MoreHoriz as MoreHorizIcon } from "@mui/icons-material";
-import CircleIcon from '@mui/icons-material/Circle';
-import ModalImage from "react-modal-image";
 import { getPosts } from "@/apis/blog/blog-repo";
-
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Blog: React.FC = () => {
     const [posts, setPosts] = useState<any[]>([]);
@@ -19,6 +18,16 @@ const Blog: React.FC = () => {
         fetchData();
     }, []);
 
+    // Cài đặt cho slider
+    const sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+    };
+
     return (
         <div className="entire-page">
             <div className="header">
@@ -29,41 +38,59 @@ const Blog: React.FC = () => {
             <div className="body">
                 <div className="column-center">
                     <div className="containe-center">
-                        {posts.map((post, index) => (
+                        {posts.map((post) => (
                             <div key={post._id} className="post-container">
                                 <div className="post-header">
                                     <div className="post-header-right">
-                                        <img 
-                                            src="https://lumiere-a.akamaihd.net/v1/images/sea-avatar-wayofwater-gallery06_a360a29e.jpeg?region=1%2C0%2C1298%2C730" 
-                                            alt="author" 
-                                            className="circle-image" 
+                                        <img
+                                            src="https://lumiere-a.akamaihd.net/v1/images/sea-avatar-wayofwater-gallery06_a360a29e.jpeg?region=1%2C0%2C1298%2C730"
+                                            alt="author"
+                                            className="circle-image"
                                         />
                                         <div className="post-header-title">
                                             <p style={{ fontSize: 20, fontWeight: "bold" }}>Tác giả</p>
                                             <div className="post-header-title-daypost">
                                                 <p style={{ fontSize: 16 }}>{new Date(post.createdAt).toLocaleDateString()}</p>
-                                                <CircleIcon style={{ color: "black", fontSize: 6, margin: "0 5px" }} />
-                                                <p style={{ fontSize: 16 }}>{new Date(post.createdAt).toLocaleTimeString()}</p>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="post-header-left" style={{ cursor: 'pointer' }}>
-                                        <MoreHorizIcon />
                                     </div>
                                 </div>
                                 <div className="post-title">
                                     <p>{post.content}</p>
                                 </div>
+
+                                {/* Hiển thị ảnh */}
                                 <div className="post-image">
-                                    {post.images.map((image, idx) => (
-                                        <ModalImage
-                                            key={idx}
-                                            small={image}
-                                            large={image}
-                                            alt={`Image ${idx + 1}`}
+                                    {post.images.length > 1 ? (
+                                        <Slider {...sliderSettings}>
+                                            {post.images.map((image, idx) => (
+                                                <img
+                                                    key={idx}
+                                                    src={image}
+                                                    alt={`Image ${idx + 1}`}
+                                                    style={{
+                                                        width: "100%",
+                                                        height: "auto",
+                                                        objectFit: "cover",
+                                                        borderRadius: "8px",
+                                                    }}
+                                                />
+                                            ))}
+                                        </Slider>
+                                    ) : (
+                                        <img
+                                            src={post.images[0]}
+                                            alt="Single Image"
+                                            style={{
+                                                width: "100%",
+                                                height: "auto",
+                                                objectFit: "cover",
+                                                borderRadius: "8px",
+                                            }}
                                         />
-                                    ))}
+                                    )}
                                 </div>
+
                                 <div className="divider-post"></div>
                             </div>
                         ))}
